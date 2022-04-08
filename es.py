4729a2d5cc909a41f62254064e7a688880f54f51
Michael <G00411301@gmit.ie>
2022-04-08 21:53:23 +0100

    Count the number of e's

---
+++ es.py
@@ -0,0 +1,34 @@
+# es.py
+# This program reads in a file and counts how many e's are in it 
+# Author Michael Casey
+
+
+import sys
+
+filename = None
+startingpoint = 0
+
+#checking to mkae sure an argument/filename has been included in the command line
+while len(sys.argv)<2:
+#if there is only one name ie. "python .\es.py the below message pops up and closes the program"
+    print ("Please include file name/path in command line")
+    quit()
+
+#I assume the second argument passed into the command line is a file/path
+filename = sys.argv[1]
+
+
+#filename = "test.txt"
+#I assume file is in current directory or command line input includes the path
+with open (filename, 'r') as f:
+   data = f.read()
+   #counting how many e's are in the text file - source geeksforgeeeks.org
+   nume = data.count('e')
+   numE = data.count('E')
+   alle = nume + numE
+   print ("There are", alle, "e's in", filename)
+   
+
+
+
+
